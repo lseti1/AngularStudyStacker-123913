554c139fe9cd5183e480d104463d13406e968ff8
Lucas Setiady <luca.setia@gmail.com>
2025-08-21 17:31:02 +1000

    Styling: Set up the 2 main parent components and the format/styling for the child components of the sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './sidebar/sidebar';
+import { MainLayout } from './main-layout/main-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, MainLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
