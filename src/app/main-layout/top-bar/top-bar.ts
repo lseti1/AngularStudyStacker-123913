6cf38fec57faa44bcbdda068de08270dc0679c64
Lucas Setiady <luca.setia@gmail.com>
2025-08-26 09:40:46 +1000

    Styling: Added FA icons around application

--- src/app/main-layout/top-bar/top-bar.ts
+++ src/app/main-layout/top-bar/top-bar.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { SelectedDeck } from '../../services/selected-deck';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './top-bar.html',
   styles: ``
 })
 export class TopBar {
+  faUser = faUser;
+  faGear = faGear;
+
   @Input() deckTitle: string = '';
   @Input() deckSize: number = 0;
   @Input() deckProficiency: number = 0;
