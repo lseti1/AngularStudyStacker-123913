f46b45f4babb7a28cf19d5d65c4f9e53c666139f
Lucas Setiady <luca.setia@gmail.com>
2025-08-26 13:53:54 +1000

    Functionality: Updated decks-list to have correct symbol and made the Study Stacker title return back to no selected deck/home screen

--- src/app/sidebar/decks-list/decks-list.ts
+++ src/app/sidebar/decks-list/decks-list.ts
@@ -1,14 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SelectedDeck } from '../../services/selected-deck';  
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-decks-list',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './decks-list.html',
   styleUrl: './decks-list.css'
 })
 export class DecksList  {
+  constructor(public selectedDeckService: SelectedDeck) {}
+  faGear = faGear;
   @Input() deckTitles: string[] = [];
 
-  constructor(public selectedDeckService: SelectedDeck) {}
+  
 }
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DecksList } from './decks-list/decks-list';
 import { AddDeckOptions } from './add-deck-options/add-deck-options';
 import { DummyDataService } from '../services/dummy-data-service';
+import { SelectedDeck } from '../services/selected-deck';
 
 
 @Component({
@@ -13,7 +14,7 @@ import { DummyDataService } from '../services/dummy-data-service';
 export class Sidebar implements OnInit {
   deckTitles: string[] = [];
 
-  constructor (public dummyDataService: DummyDataService) {}
+  constructor (public dummyDataService: DummyDataService, public selectedDeckService: SelectedDeck) {}
 
   ngOnInit() {
     this.deckTitles = this.dummyDataService.getDeckTitles();
