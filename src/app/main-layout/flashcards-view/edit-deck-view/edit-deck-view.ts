52e7a503f1332f1baeec16f8d91490882cf91b11
Lucas Setiady <luca.setia@gmail.com>
2025-08-28 09:29:23 +1000

    Fix: Button was missing on the edit-deck-view. How updated with simple onSubmit() function that may need to be adjusted to reopen the selected deck (not like add-deck)

--- src/app/main-layout/flashcards-view/edit-deck-view/edit-deck-view.ts
+++ src/app/main-layout/flashcards-view/edit-deck-view/edit-deck-view.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Deck } from '../../../services/dummy-data-service';
-
+import { SelectedDeck } from '../../../services/selected-deck';
 @Component({
   selector: 'app-edit-deck-view',
   imports: [],
@@ -8,5 +8,10 @@ import { Deck } from '../../../services/dummy-data-service';
   styleUrl: './edit-deck-view.css'
 })
 export class EditDeckView {
+  constructor(public selectedDeckService: SelectedDeck) {}
   @Input() deckData: Deck | null = null;
+
+  onSubmit() {
+    this.selectedDeckService.setAddingDeck(false);
+  }
 }
