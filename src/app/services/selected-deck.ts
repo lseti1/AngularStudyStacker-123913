2ea04a902abd013bf69e6abd3ac2744200fc7aed
Lucas Setiady <luca.setia@gmail.com>
2025-09-01 10:18:20 +1000

    Code: Refactoring code and implementing types inside services to cleaner code, cleaned up action-bar if/else block

--- src/app/services/selected-deck.ts
+++ src/app/services/selected-deck.ts
@@ -1,35 +1,25 @@
 import { Injectable } from '@angular/core';
 import { signal } from '@angular/core';
 
+type selectedDeckArea = 'add' | 'edit' | 'delete' | null;
+
 @Injectable({ providedIn: 'root' })
 export class SelectedDeck {
-  // isAddingDeck = signal<boolean>(false);
-  // isEditingDeck = signal<boolean>(false);
   isDeckSelected = signal<boolean>(false);
   selectedDeckID = signal<number | null>(null);
 
-  private selectedDeckState = signal< 'add' | 'edit' | 'delete' | null>('delete');
+  private selectedDeckState = signal<selectedDeckArea>(null);
 
   get currentView() {
     return this.selectedDeckState.asReadonly();
   }
 
-  toggleView(view: 'add' | 'edit' | 'delete' | null) {
+  toggleView(view: selectedDeckArea) {
     this.selectedDeckState.set(view);
   }
-
-  // setAddingDeck(value: boolean) {
-  //   this.isAddingDeck.set(value);
-  // }
-
-  // setEditingDeck(value: boolean) {
-  //   this.isEditingDeck.set(value);
-  // }
 
   setSelectedDeck(selected: boolean, deckID: number) {
     this.isDeckSelected.set(selected);
     this.selectedDeckID.set(deckID);
-    console.log("Selected Deck:", this.isDeckSelected());
-    console.log("Selected Deck ID:", this.selectedDeckID());
   }
 }
