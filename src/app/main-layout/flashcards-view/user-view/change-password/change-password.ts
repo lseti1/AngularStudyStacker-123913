36b244d626aff98d6bb229852970754f1e8c5e81
Lucas Setiady <luca.setia@gmail.com>
2025-09-01 14:45:45 +1000

    Components: Completed linking & styling for change-password component

--- src/app/main-layout/flashcards-view/user-view/change-password/change-password.ts
+++ src/app/main-layout/flashcards-view/user-view/change-password/change-password.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UIStates } from '../../../../services/ui-states';
+import { UiStatesUser } from '../../../../services/ui-states-user';
 
 @Component({
   selector: 'app-change-password',
@@ -7,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './change-password.css'
 })
 export class ChangePassword {
+  constructor(public uiStates: UIStates, public uiStatesUser: UiStatesUser) {}
+  
+  onCancel() {
+    this.uiStatesUser.toggleView('userAccount');
+  }
 
+  onSubmit() {
+    this.uiStatesUser.toggleView('login');
+    this.uiStates.toggleView(null);
+  }
 }
