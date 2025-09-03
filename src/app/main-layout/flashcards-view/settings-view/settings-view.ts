e1d5cff4c8cf4eff74c4f4c63c4bc2b251c168ef
Lucas Setiady <luca.setia@gmail.com>
2025-09-03 12:56:58 +1000

    Functionality: Added button that allows user to clear all app data e.g. delete all decks/cards

--- src/app/main-layout/flashcards-view/settings-view/settings-view.ts
+++ src/app/main-layout/flashcards-view/settings-view/settings-view.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UIStates } from '../../../services/ui-states';
+import { LocalStorageService } from '../../../services/local-storage-service';
 
 @Component({
   selector: 'app-settings-view',
@@ -9,9 +10,14 @@ import { UIStates } from '../../../services/ui-states';
   styleUrl: './settings-view.css'
 })
 export class SettingsView {
-  constructor(public uiStates: UIStates) {}
+  constructor(public uiStates: UIStates, public localStorageService: LocalStorageService) {}
 
   onSubmit() {
+    this.uiStates.toggleView(null);
+  }
+
+  onClearData() {
+    this.localStorageService.clearAppData();
     this.uiStates.toggleView(null);
   }
 }
