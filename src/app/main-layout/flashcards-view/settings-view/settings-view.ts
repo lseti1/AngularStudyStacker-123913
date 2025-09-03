5a9f51462770e7b7a11dac46b256a1fe7fe86c73
Lucas Setiady <luca.setia@gmail.com>
2025-09-03 13:40:54 +1000

    Functionality: Improved account settings UI and also added an indicator to make it clearer that data is being deleted

--- src/app/main-layout/flashcards-view/settings-view/settings-view.ts
+++ src/app/main-layout/flashcards-view/settings-view/settings-view.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UIStates } from '../../../services/ui-states';
 import { LocalStorageService } from '../../../services/local-storage-service';
@@ -12,12 +12,18 @@ import { LocalStorageService } from '../../../services/local-storage-service';
 export class SettingsView {
   constructor(public uiStates: UIStates, public localStorageService: LocalStorageService) {}
 
+  message = signal<string>("Clear App Data");
+
   onSubmit() {
     this.uiStates.toggleView(null);
   }
 
   onClearData() {
-    this.localStorageService.clearAppData();
-    this.uiStates.toggleView(null);
+    this.message.set("Clearing...");
+
+    setTimeout(() => {
+      this.localStorageService.clearAppData();
+      this.uiStates.toggleView(null);
+    }, 2000);
   }
 }
