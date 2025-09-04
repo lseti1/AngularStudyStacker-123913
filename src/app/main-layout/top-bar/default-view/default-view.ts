77c5fee4dfe75983e3f2df2d82e661d402d70842
Lucas Setiady <luca.setia@gmail.com>
2025-09-04 11:17:03 +1000

    Functionality: Updated the topBar to have two components which correspond to whether the app is learning or not (two separate views to show different details)

---
+++ src/app/main-layout/top-bar/default-view/default-view.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { SelectedDeck } from '../../../services/selected-deck';
+import { UIStates } from '../../../services/ui-states';
+import { FlashcardsLearning } from '../../../services/flashcards-learning';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGear, faUser } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
+import { TruncatePipe } from '../../../pipes/truncate-pipe';
+
+@Component({
+  selector: 'app-default-view',
+  imports: [FontAwesomeModule, CommonModule, TruncatePipe],
+  templateUrl: './default-view.html',
+  styleUrl: './default-view.css'
+})
+export class DefaultView {
+  constructor(
+      public selectedDeckService: SelectedDeck,
+      public uiStates: UIStates,
+      public flashcardsLearningService: FlashcardsLearning
+    ) {}
+
+  faUser = faUser;
+  faGear = faGear;
+  currentDate: Date = new Date();
+
+  @Input() deckTitle: string = '';
+  @Input() deckSize: number = 0;
+  @Input() deckProficiency: number = 0;
+  @Input() totalDecks: number = 0;
+}
