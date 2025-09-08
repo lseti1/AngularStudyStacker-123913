a0e698964854d99b96ee8213798a0913f3386f96
Lucas Setiady <luca.setia@gmail.com>
2025-09-08 16:54:37 +1000

    Form Validation: Updated the delete account section to check for password and also only allow the delete account button to become available if the entered password matches the demo password

--- src/app/main-layout/flashcards-view/user-view/delete-account/delete-account.ts
+++ src/app/main-layout/flashcards-view/user-view/delete-account/delete-account.ts
@@ -1,22 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { UiStatesUser } from '../../../../services/ui-states-user';
 import { UIStates } from '../../../../services/ui-states';
+import { DummyDataService } from '../../../../services/dummy-data-service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-delete-account',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './delete-account.html',
   styleUrl: './delete-account.css'
 })
 export class DeleteAccount {
-  constructor(public uiStates: UIStates, public uiStatesUser: UiStatesUser) {}
+  enteredPassword: string = '';
+  demoPassword: string;
+  invalidPassword = signal<boolean>(true);
+  message: string = 'Note: Deleting accounts is not implemented at this time. This page is for demonstrating page functionality & navigation only.';
+  deleteMessage: string = 'Delete Account';
+
+  constructor(
+    public uiStates: UIStates, 
+    public uiStatesUser: UiStatesUser,
+    public dummyDataService: DummyDataService
+  ) { this.demoPassword = this.dummyDataService.getDemoUserCredentials().password; }
 
   onCancel() {
     this.uiStatesUser.toggleView('userAccount');
   }
 
-  onSubmit() {
-    this.uiStatesUser.toggleView('login');
-    this.uiStates.toggleView(null);
+  checkPassword(value: string) {
+    this.invalidPassword.set(value !== this.demoPassword);
+  }
+
+  onSubmit(formData: NgForm) {
+    if (this.demoPassword !== this.enteredPassword) {
+      this.invalidPassword.set(true);
+      return;
+    } 
+    this.invalidPassword.set(false);
+    
+    this.deleteMessage = 'Redirecting...';
+    this.message = "Simulating account deletion."
+
+    setTimeout(() => {
+      this.uiStatesUser.toggleView('login');
+      this.uiStates.toggleView(null);
+      this.invalidPassword.set(true);
+    }, 5000);  
   }
 }
