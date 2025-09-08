9681541da9f049c07d4a285a8ae675bfed243349
Lucas Setiady <luca.setia@gmail.com>
2025-09-08 15:43:52 +1000

    Form Validation: The forgot password view has now been updated to check email correctly and notify user

--- src/app/main-layout/flashcards-view/user-view/forgot-password/forgot-password.ts
+++ src/app/main-layout/flashcards-view/user-view/forgot-password/forgot-password.ts
@@ -1,17 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { UIStates } from '../../../../services/ui-states';
 import { UiStatesUser } from '../../../../services/ui-states-user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.css'
 })
 export class ForgotPassword {
-  message: string = 'We will send a password reset link to the email entered above if an account is registered with that email';
+  enteredEmail: string = '';
   resetMessage: string = 'Send Reset Link';
+  message: string = 'Note: Resetting passwords is not implemented at this time. This page is for demonstrating page functionality & navigation only.';
 
   constructor(
     public uiStates : UIStates, 
@@ -19,7 +21,7 @@ export class ForgotPassword {
   ) {}
   
 
-  onSendReset() {
+  onSendReset(formData: NgForm) {
     this.message = 'Password reset link has been sent.';
     this.resetMessage = 'Redirecting...';
 
