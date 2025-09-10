61dbe39a67312febf837e0a9bc0a5b51d2beeadf
Lucas Setiady <luca.setia@gmail.com>
2025-09-10 11:16:58 +1000

    Code: Refactored & cleaned up code for the top bar functionality. Removing unused imports, variables etc. Making services private, modifiers, etc.

--- src/app/main-layout/top-bar/learning-view/learning-view.ts
+++ src/app/main-layout/top-bar/learning-view/learning-view.ts
@@ -1,10 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { SelectedDeck } from '../../../services/selected-deck';
-import { UIStates } from '../../../services/ui-states';
 import { FlashcardsLearning } from '../../../services/flashcards-learning';
 import { TruncatePipe } from '../../../pipes/truncate-pipe';
 import { DatePipe } from '@angular/common';
-import { UiStatesSettings } from '../../../services/ui-states-settings';
 import { LocalStorageService } from '../../../services/local-storage-service';
 
 @Component({
@@ -14,15 +11,22 @@ import { LocalStorageService } from '../../../services/local-storage-service';
   styleUrl: './learning-view.css'
 })
 export class LearningView {
-  cardsPerSession: number;
+  @Input() deckTitle: string = '';
+  public cardsPerSession: number;
 
   constructor(
-    public flashcardsLearningService: FlashcardsLearning,
-    public localStorageService: LocalStorageService
+    private flashcardsLearningService: FlashcardsLearning,
+    private localStorageService: LocalStorageService
   ) {
     const settings = this.localStorageService.getSettings();
     this.cardsPerSession = settings.cardsPerSession;
   }
 
-  @Input() deckTitle: string = '';
+  public get timer(): number {
+    return this.flashcardsLearningService.timeCount() * 1000;
+  }
+
+  public get learntCount(): number {
+    return this.flashcardsLearningService.flashcardsLearntCount();
+  }
 }
--- src/app/main-layout/top-bar/top-bar.ts
+++ src/app/main-layout/top-bar/top-bar.ts
@@ -1,10 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SelectedDeck } from '../../services/selected-deck';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faGear } from '@fortawesome/free-solid-svg-icons';
-import { UIStates } from '../../services/ui-states';
 import { FlashcardsLearning } from '../../services/flashcards-learning';
 import { DefaultView } from './default-view/default-view';
 import { LearningView } from './learning-view/learning-view';
@@ -16,18 +12,16 @@ import { LearningView } from './learning-view/learning-view';
   styles: ``
 })
 export class TopBar {
-  constructor(
-    public selectedDeckService: SelectedDeck,
-    public uiStates: UIStates,
-    public flashcardsLearningService: FlashcardsLearning
-  ) {}
-
-  faUser = faUser;
-  faGear = faGear;
-  currentDate: Date = new Date();
-
   @Input() deckTitle: string = '';
   @Input() deckSize: number = 0;
   @Input() deckProficiency: number = 0;
   @Input() totalDecks: number = 0;
+
+  constructor(
+    private flashcardsLearningService: FlashcardsLearning
+  ) {}
+
+  public get isLearning(): Signal<boolean> {
+    return this.flashcardsLearningService.isLearning;
+  }
 }
