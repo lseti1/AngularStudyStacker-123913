5e6b5396064dd5c8f4b82914a734e0a4f57dae56
Lucas Setiady <luca.setia@gmail.com>
2025-09-10 15:16:39 +1000

    Form Validation: Added max length for deck name and description, also added error messages in those cases and also if they try adding another deck with the same name

--- src/app/main-layout/flashcards-view/add-deck-view/add-deck-view.ts
+++ src/app/main-layout/flashcards-view/add-deck-view/add-deck-view.ts
@@ -1,28 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SelectedDeck } from '../../../services/selected-deck';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Deck, Flashcard } from '../../../services/dummy-data-service';
 import { LocalStorageService } from '../../../services/local-storage-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-deck-view',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-deck-view.html',
   styleUrl: './add-deck-view.css'
 })
 export class AddDeckView {
+  public errorMessage: string = '';
   public deckName: string = '';
   public deckDescription: string ='';
+  public showErrorMessage = signal<boolean>(false);
+
   private deckProficiency: number = 0;
   private deckFlashcards: Flashcard[] = [];
+  private deckTitles: string[] = [];
 
   constructor(
     private selectedDeckService: SelectedDeck, 
     private localStorageService: LocalStorageService
-  ) {}
+  ) {
+    this.deckTitles = localStorageService.getDeckTitles();
+  }
 
-  onSubmit(): void {
-    if (!this.deckDescription || !this.deckName) return;
+  onSubmit(formData: NgForm): void {
+    if (this.deckTitles.includes(this.deckName)) {
+      this.setTemporaryMessage(`'${this.deckName}' is already taken. Please enter another name for your deck.`);
+      formData.reset();
+      this.deckName = '';
+      this.deckDescription = '';
+      return;
+    } 
 
     const newDeck: Deck = {
       id: this.localStorageService.getDeckID(),
@@ -32,7 +45,17 @@ export class AddDeckView {
       flashcards: this.deckFlashcards
     };
 
+    formData.reset();
     this.localStorageService.addDeck(newDeck);
     this.selectedDeckService.toggleView(null);
+  }
+
+  setTemporaryMessage(message: string) {
+    this.errorMessage = message;
+    this.showErrorMessage.set(true);
+
+    setTimeout(() => {
+      this.showErrorMessage.set(false);
+    }, 5000);
   }
 }
